eccbef1d6ddbb026911b4b32b76d098faa39d737
MarinaRaskevich <marina.raskevich19@gmail.com>
2024-03-10 20:55:41 +0100

    week15 star

---
+++ index.js
@@ -0,0 +1,19 @@
+let numbers = [];
+
+//Заполните массив numbers целыми числами от -10 до 10 (включительно) с помощью цикла.
+// for (let number = -10; number <= 10; number++) {
+//   numbers.push(number);
+// }
+// console.log(numbers);
+
+//Удалите все отрицательные числа из массива
+// numbers = numbers.filter((number) => number >= 0);
+// console.log(numbers);
+
+//Возведите каждое число в квадрат
+// numbers = numbers.map((number) => number * number);
+// console.log(numbers);
+
+//Отсортируйте числа в порядке убывания
+// numbers.sort((a, b) => b - a);
+// console.log(numbers);
